c5e1ee6485b204a57bf5cbc4a66143c0a661b641
sunilkrishna49 <sunilkrishna49@gmail.com>
2024-10-14 20:26:24 +0100

    initial commit

--- src/app/base-urls.ts
+++ src/app/base-urls.ts
@@ -1,5 +1,6 @@
 export class BaseUrls {
-  public static readonly BASE_HREF: string = 'http://localhost:9090';
+
+  public static readonly BASE_HREF: string = "http://localhost:8080";
 
   public static readonly ADMIN_GROUPURL: string = 'adminauth';
   public static readonly USER_GROUPURL: string = 'users';
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'http://http://18.130.173.83/:9090',
 };
 
 /*
