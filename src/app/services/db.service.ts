cee3e3502fb625b152aa30f62997d6dc0891f48a
sunilkrishna49 <sunilkrishna49@gmail.com>
2024-10-15 12:17:45 +0100

    second commit

--- src/app/services/db.service.ts
+++ src/app/services/db.service.ts
@@ -13,67 +13,68 @@ export class Response {
   data: any[] = [];
 }
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DbService {
-
   categoies: BehaviorSubject<Category[]> = new BehaviorSubject<Category[]>([]);
   categoriesRetreivedBool: boolean = false;
-  
+
   products: BehaviorSubject<Products[]> = new BehaviorSubject<Products[]>([]);
   productsRetreivedBool: boolean = false;
-  
+
   users: BehaviorSubject<Users[]> = new BehaviorSubject<Users[]>([]);
   usersRetreivedBool: boolean = false;
 
-  shipments: BehaviorSubject<Shipments[]> = new BehaviorSubject<Shipments[]>([]);
+  shipments: BehaviorSubject<Shipments[]> = new BehaviorSubject<Shipments[]>(
+    []
+  );
   shipmentRetreivedBool: boolean = false;
 
-  constructor(
-    private httpClient: HttpClient
-  ) { }
+  constructor(private httpClient: HttpClient) {}
 
   getProducts() {
-    this.httpClient.get(BaseUrls.getUrl(BaseUrls.PRODUCT_GROUPURL))
-    .subscribe({
-      next: ({code, data, message}: any) => {          
+    this.httpClient.get(BaseUrls.getUrl(BaseUrls.PRODUCT_GROUPURL)).subscribe({
+      next: ({ code, data, message }: any) => {
         this.products.next(data);
-        this.productsRetreivedBool = true;          
+        this.productsRetreivedBool = true;
       },
       error: (error) => {
         console.log(error);
-      }
-    })
+      },
+    });
   }
 
   getCategories() {
-    this.httpClient.get(BaseUrls.getUrl(BaseUrls.CATEGORIES_GROUPURL))
-    .subscribe({
-      next: ({code, data, message}: any) => {
-        this.categoies.next(data);
-        this.categoriesRetreivedBool = true;          
-      },
-      error: (error) => {
-        console.log(error);
-      }
-    })
+    this.httpClient
+      .get(BaseUrls.getUrl(BaseUrls.CATEGORIES_GROUPURL))
+      .subscribe({
+        next: ({ code, data, message }: any) => {
+          this.categoies.next(data);
+          this.categoriesRetreivedBool = true;
+        },
+        error: (error) => {
+          console.log(error);
+        },
+      });
   }
 
   getUsers() {
-    this.httpClient.get(BaseUrls.getUrl(BaseUrls.USER_GROUPURL))
-    .subscribe({
-      next: ({code, data, message}: any) => {
+    this.httpClient.get(BaseUrls.getUrl(BaseUrls.USER_GROUPURL)).subscribe({
+      next: ({ code, data, message }: any) => {
         this.users.next(data);
-        this.usersRetreivedBool = true;          
+        this.usersRetreivedBool = true;
       },
       error: (error) => {
         console.log(error);
-      }
-    })
+      },
+    });
   }
 
   getShipments(shipmentStatus: number = 0) {
-    this.httpClient.get<Response>(`${BaseUrls.getUrl(BaseUrls.SHIPMENT_GROUPURL)}/${shipmentStatus}`)
+    this.httpClient
+      .get<Response>(
+        `${BaseUrls.getUrl(BaseUrls.SHIPMENT_GROUPURL)}/${shipmentStatus}`
+      )
       .subscribe({
         next: ({ data }) => {
           this.shipments.next(data);
@@ -81,7 +82,7 @@ export class DbService {
         },
         error: (error) => {
           console.log(error);
-        }
-      })
+        },
+      });
   }
 }
